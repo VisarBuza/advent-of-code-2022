18ed23a781eabab65b6b4e41ca0610325283441a
visar <visar@gjirafa.com>
2022-12-01 15:44:27 +0100

    loaded up on snacks

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,33 @@
 defmodule AdventOfCode.Day01 do
   def part1(_args) do
+    input = AdventOfCode.Input.get!(1)
+
+    input
+    |> String.split("\n\n")
+    |> Enum.map(&String.split(&1, "\n"))
+    |> Enum.map(fn elf ->
+      elf
+      |> Enum.filter(fn calories -> calories != "" end)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
+    |> Enum.max()
   end
 
   def part2(_args) do
+    input = AdventOfCode.Input.get!(1)
+
+    input
+    |> String.split("\n\n")
+    |> Enum.map(&String.split(&1, "\n"))
+    |> Enum.map(fn elf ->
+      elf
+      |> Enum.filter(fn calories -> calories != "" end)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
+    |> Enum.sort(fn a, b -> a > b end)
+    |> Enum.take(3)
+    |> Enum.sum()
   end
 end
